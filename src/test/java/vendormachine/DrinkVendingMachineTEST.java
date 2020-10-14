e12bb2df73615f174676ab02d09d96782f5f7ce4
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-14 16:57:43 +0100

    80% on drinkvendingmachine

--- src/test/java/vendormachine/DrinkVendingMachineTEST.java
+++ src/test/java/vendormachine/DrinkVendingMachineTEST.java
@@ -1,9 +1,12 @@
 package vendormachine;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,7 +14,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import vendormachine.users.Person;
 import vendormachine.users.util.Wallet;
+import vendormachine.vendors.DefaultSnacks;
 import vendormachine.vendors.DrinkVendingMachine;
 import vendormachine.vendors.enums.BRANDS;
 import vendormachine.vendors.item.Snack;
@@ -20,9 +25,14 @@ public class DrinkVendingMachineTEST {
 	
 
 	private BRANDS brandName = null;
-	private float availebleCredit = 0;
+	private DefaultSnacks snackList;
+	private Wallet wallet;
+	private Person user;
+	private DrinkVendingMachine dvm;
+	private float availableCredit = 0;
 	private ArrayList<Snack> drinkList = new ArrayList<Snack>();
 	private String div = "-----";
+	private String name = "test";
 	
 
 	@BeforeClass
@@ -32,16 +42,59 @@ public class DrinkVendingMachineTEST {
 	
 	@Before
 	public void beforeTest() {
-		System.out.println("Before");
+		this.dvm = new DrinkVendingMachine();
+		this.wallet = new Wallet();
+		this.wallet.setCredit(availableCredit);
+		this.user = new Person(this.name, this.wallet);
 	}
 	
 	@Test
 	public void testDrinkVendingMachine() {
-		DrinkVendingMachine dvm = new DrinkVendingMachine();
-		assertTrue ()
+		assertTrue (dvm instanceof DrinkVendingMachine);
 		
+		dvm = new DrinkVendingMachine(availableCredit, brandName);
+		assertTrue (dvm instanceof DrinkVendingMachine);
 	}
 	
+	@Test
+	public void testPopulateDrinkList() {
+		drinkList.addAll(Arrays.asList(DefaultSnacks.snackList));
+		assertTrue(drinkList != null);
+		assertTrue(!drinkList.isEmpty());
+	}
+	
+	@Test
+	public void testSelectDrink() {
+		availableCredit = 10;
+		this.user.addCredit(availableCredit);
+		this.dvm.giveCredit(this.user, availableCredit);
+		assertEquals(DefaultSnacks.snackList[1], this.dvm.selectDrink(1));
+	}
+	
+	@Test
+	public void testGiveCredit() {
+		this.dvm.giveCredit(this.user, this.availableCredit);
+		assertTrue(this.user.getWallet().getAllCredit() == availableCredit);
+	}
+	
+	@Test
+	public void testSetGetBrandName() {
+		this.dvm.setBrandName(brandName);
+		assertTrue(dvm.getBrandName() == this.brandName);
+	}
+	
+	@Test
+	public void testGetAvailableCredit() {
+		this.dvm.setAvailableCredit(availableCredit);
+		assertTrue(dvm.getAvailableCredit() == this.availableCredit);
+		
+	}
+		
+	@Test
+	public void testSetGetDrinkList() {
+		this.dvm.giveCredit(user, availableCredit);
+		assertEquals(null, this.dvm.selectDrink(0));
+	}
 	
 	@After
 	public void afterTest() {
