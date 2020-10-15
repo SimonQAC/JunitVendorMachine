06157f20fa6dd7a09d972a28f03b0a4cce0d6241
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-15 09:37:56 +0100

    add ExtentReports

--- src/test/java/vendormachine/DrinkVendingMachineTEST.java
+++ src/test/java/vendormachine/DrinkVendingMachineTEST.java
@@ -76,51 +76,67 @@ public class DrinkVendingMachineTEST {
 	
 	@Test
 	public void testPopulateDrinkList() {
+		test = report.startTest("testPopulateDrinkList");
 		drinkList.addAll(Arrays.asList(DefaultSnacks.snackList));
+		testReport.assertReport(test, true, drinkList != null);
 		assertTrue(drinkList != null);
+		testReport.assertReport(test, true, !drinkList.isEmpty());
 		assertTrue(!drinkList.isEmpty());
 	}
 	
 	@Test
 	public void testSelectDrink() {
+		test = report.startTest("testSelectDrink");
 		availableCredit = 10;
 		this.user.addCredit(availableCredit);
 		this.dvm.giveCredit(this.user, availableCredit);
+		testReport.assertReport(test, true, DefaultSnacks.snackList[1] == this.dvm.selectDrink(1));
 		assertEquals(DefaultSnacks.snackList[1], this.dvm.selectDrink(1));
 	}
 	
 	@Test
 	public void testGiveCredit() {
+		test = report.startTest("testGiveCredit");
 		this.dvm.giveCredit(this.user, this.availableCredit);
+		testReport.assertReport(test, true, this.user.getWallet().getAllCredit() == availableCredit);
 		assertTrue(this.user.getWallet().getAllCredit() == availableCredit);
 	}
 	
 	@Test
 	public void testSetGetBrandName() {
+		test = report.startTest("testSetGetBrandName");
 		this.dvm.setBrandName(brandName);
+		testReport.assertReport(test, true, dvm.getBrandName() == this.brandName);
 		assertTrue(dvm.getBrandName() == this.brandName);
 	}
 	
 	@Test
 	public void testGetAvailableCredit() {
+		test = report.startTest("testGetAvailableCredit");
 		this.dvm.setAvailableCredit(availableCredit);
+		testReport.assertReport(test, true, dvm.getAvailableCredit() == this.availableCredit);
 		assertTrue(dvm.getAvailableCredit() == this.availableCredit);
 		
 	}
 		
 	@Test
 	public void testSetGetDrinkList() {
+		test = report.startTest("testSetGetDrinkList");
 		this.dvm.giveCredit(user, availableCredit);
+		testReport.assertReport(test, true, null == this.dvm.selectDrink(0));
 		assertEquals(null, this.dvm.selectDrink(0));
 	}
 	
 	@After
 	public void afterTest() {
 		System.out.println(div);
+		report.endTest(test);
 	}
 	
 	@AfterClass
 	public static void afterTestSuite() {
 		System.out.println("AfterClass: End of TestSuite");
+		report.flush();
+		report.close();
 	}
 }
--- src/test/java/vendormachine/WalletTEST.java
+++ src/test/java/vendormachine/WalletTEST.java
@@ -3,11 +3,18 @@ package vendormachine;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 
 import vendormachine.users.util.Wallet;
 
@@ -19,10 +26,20 @@ public class WalletTEST {
 	private static int activeTest = 1;
 	private static StringBuilder sBuilder = new StringBuilder();
 	private static String div = "-----";
+	
+	private static ExtentReports report;
+	private ExtentTest test;
 
 	@BeforeClass
 	public static void beforeTestSuite() {
 		System.out.println("BeforeClass: Start of TestSuite\n");
+		
+		Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
+		Path filePath = Paths.get(root.toString(), "\\target\\reports\\WalletReport.html");
+		
+		System.out.println(filePath.toString());
+		
+		report = new ExtentReports(filePath.toString(), true);
 	}
 	
 	@Before
@@ -32,63 +49,82 @@ public class WalletTEST {
 	
 	@Test
 	public void testWalletConstructor() {
+		test = report.startTest("testWalletConstructor");
 		Wallet wallet = new Wallet();
+		testReport.assertReport(test, true, wallet instanceof Wallet);
 		assertTrue (wallet instanceof Wallet);
 		
 		wallet = new Wallet(storedCredit);
+		testReport.assertReport(test, true, wallet instanceof Wallet);
 		assertTrue (wallet instanceof Wallet);
 		
 		wallet = new Wallet (brand, storedCredit);
+		testReport.assertReport(test, true, wallet instanceof Wallet);
 		assertTrue (wallet instanceof Wallet);
 	}
 	
 	@Test
 	public void testCredit() {
+		test = report.startTest("testCredit");
 		Wallet wallet = new Wallet(storedCredit);
 		wallet.addCredit(storedCredit);
+		testReport.assertReport(test, true, 1.0 == wallet.getAllCredit());
 		assertEquals(1.0, wallet.getAllCredit(),0);
 	}
 	
 	@Test
 	public void testGetBrand() {
+		test = report.startTest("testGetBrand");
 		Wallet wallet = new Wallet(brand, storedCredit);
+		testReport.assertReport(test, true, brand == wallet.getBrand());
 		assertEquals(brand, wallet.getBrand());
 	}
 	
 	@Test
 	public void testGetCredit() {
-		Wallet wallet = new Wallet(storedCredit);
-		wallet.getCredit((float) 0.1);
+		test = report.startTest("testGetCredit");
+		Wallet wallet = new Wallet();
+		testReport.assertReport(test, true, 0 == wallet.getCredit(0));
+		assertEquals(0, wallet.getCredit(0), 0);
 	}
 	
 	@Test
 	public void testSetBrand() {
+		test = report.startTest("testSetBrand");
 		Wallet wallet = new Wallet();
 		wallet.setBrand(brand);
+		testReport.assertReport(test, true, brand == wallet.getBrand());
 		assertEquals(brand, wallet.getBrand());
 	}
 
 	@Test
 	public void testSetCredit() {
+		test = report.startTest("testSetCredit");
 		Wallet wallet = new Wallet();
 		wallet.setCredit(storedCredit);
+		testReport.assertReport(test, true, storedCredit == wallet.getAllCredit());
 		assertEquals(storedCredit, wallet.getAllCredit(), 0);
 	}
 	
 	@Test 
 	public void testInsufficientFunds() {
+		test = report.startTest("testInsufficientFunds");
 		Wallet wallet = new Wallet(brand, storedCredit);
+		testReport.assertReport(test, true, 0f == wallet.getCredit(20.0f));
 		assertEquals(0f, wallet.getCredit(20.0f), 0.1f);
 	}
 	
 	@After
 	public void afterTest() {
 		System.out.println(div);
+		report.endTest(test);
 	}
 	
 	@AfterClass
 	public static void afterTestSuite() {
 		System.out.println("AfterClass: End of TestSuite");
+		report.flush();
+		report.close();
 	}
 }
 
